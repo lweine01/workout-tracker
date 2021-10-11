9e70587dae2380fdee077c94dcd51491f743bd5a
lweine01 <laurarweiner@gmail.com>
2021-10-11 17:35:47 -0400

    updated api routes based on the api.js in the public file.

---
+++ routes/api.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+
+router.get("/api/workout", (req, res) => {
+    Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+});
+
+router.post("/api/workouts/", ({ body }, res) => {
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.findByIdAndUpdate(req.params.id, {$push: {exercises: body}}, {new: true})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .limit(7)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
